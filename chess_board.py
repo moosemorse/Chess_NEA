a8df1ba243b2f6b588d51aba64d223397b803e5c
moosemorse <aivaylovivanov@gmail.com>
2023-09-01 19:34:40 +0100

    update chess_board

--- chess_board.py
+++ chess_board.py
@@ -1,2 +1,32 @@
-print("hello") 
+import pygame 
 
+pygame.init() 
+#display window, set dimensions 800x800 
+screen = pygame.display.set_mode([800, 800]) 
+#font size variable which we can adjust later 
+font_size = 20 
+font = pygame.font.SysFont('monospace', font_size) 
+#controls the speed at which the game runs at 
+timer = pygame.time.Clock() 
+fps = 60 
+
+#game variables and images 
+
+
+#main game loop 
+run = True 
+while run: 
+    timer.tick(fps) 
+    #background colour of chess board 
+    screen.fill('dark gray') 
+
+    #event handling 
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: 
+            run = False
+    
+
+
+    pygame.display.flip() 
+
+pygame.quit() 
