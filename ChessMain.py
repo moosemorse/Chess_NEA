9723e0bb68ffc8cf7866e29a83c6b04afdf69f9c
moosemorse <aivaylovivanov@gmail.com>
2023-10-26 15:02:16 +0100

    undo moves and validation for valid moves

--- ChessMain.py
+++ ChessMain.py
@@ -33,16 +33,60 @@ def main():
 
     # Import game state from ChessEngine file 
     state = ChessEngine.GameState() 
-
+    # Store a list of valid moves 
+    validMoves = state.getValidMoves() 
+    # Flag variable after a move is made 
+    moveMade = False
     # Load all assets into IMAGES dictionary 
     loadImages() 
-
+    # 'sqSelected' keeps track of last position selected by user 
+    sqSelected = ()
+    # 'playerClicks' keeps track of player clicks, last pos to new pos 
+    playerClicks= []
     run = True 
     while run: 
         for event in pygame.event.get(): 
             if event.type == pygame.QUIT: 
                 run = False 
-        
+            # Mouse handler 
+            elif event.type == pygame.MOUSEBUTTONDOWN: 
+                # Get location of mouse 
+                location = pygame.mouse.get_pos()
+                col = location[0]//SQ_SIZE
+                row = location[1]//SQ_SIZE
+                # Check if user selected same square twice 
+                if sqSelected == (row,col): 
+                    # Deselect square and clear player clicks 
+                    sqSelected = () 
+                    playerClicks = [] 
+                else: 
+                    sqSelected = (row, col) 
+                    # Append for both 1st and 2nd clicks 
+                    playerClicks.append(sqSelected) 
+                # Check if piece has been moved to another square 
+                if len(playerClicks) == 2: 
+                    move = ChessEngine.Move(playerClicks[0], playerClicks[1], state.board)
+                    # Print notation of move (start --> end)
+                    print(move.getNotation())
+                    if move in validMoves: 
+                        # Update gamestate by making move 
+                        state.makeMove(move) 
+                        moveMade = True 
+                    # Reset user clicks 
+                    sqSelected = () 
+                    playerClicks = [] 
+
+            # Key handler 
+            elif event.type == pygame.KEYDOWN: 
+                # If key 'z' is pressed, execute undoMove 
+                if event.key == pygame.K_z: 
+                    state.undoMove()
+                    moveMade = True  
+
+        if moveMade: 
+            validMoves = state.getValidMoves() 
+            moveMade = False 
+
         # Draw the current board 
         drawGameState(screen, state, IMAGES, SQ_SIZE)  
         clock.tick(FPS) 
